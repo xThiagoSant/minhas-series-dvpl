d9d42f09e6c7aee866d7528c01b44fe0311a6258
xthiagosant <uzmaki_thiago@outlook.com>
2020-06-13 11:07:31 -0300

    adicionado Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler
+} from 'reactstrap'
+
 
 function App() {
+  const [colapsed, setColapsed] = useState(false)
+  const toggleColapsed = () => setColapsed(!colapsed)
   return (
-    <div className="App">
-    <h1>Hello, world.</h1>
+    <div>
+      <Navbar color='light' light expand='md'>
+        <NavbarBrand>Minhas Séries</NavbarBrand>
+        <NavbarToggler onClick={toggleColapsed}/>
+        <Collapse isOpen={colapsed} navbar>
+          <Nav className='ml-auto' navbar>
+            <NavItem>
+              <NavLink href='/'>Gêneros</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse> 
+      </Navbar>
     </div>
   );
 }
 
 export default App;
+// esse app vai usar bootstrap: yarn add bootstrap
+// e para as interações com ele vamos adicionar um modulo chamada reacstrap: yarn add reactstrap
+// sempre que não existe a pasta node_modules: yarn ou npm install
